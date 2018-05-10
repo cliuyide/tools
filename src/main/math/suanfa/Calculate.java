2a03100625e073739f39d7dc4f6fe5fa70f0127a
liuyi <www.934392789@qq.com>
2018-05-10 09:18:45 +0800

    算法1

--- src/main/math/suanfa/Calculate.java
+++ src/main/math/suanfa/Calculate.java
@@ -12,53 +12,57 @@ public class Calculate {
         Stack<Integer> numberStack = new Stack<>();
         Stack<Character> operatorStack = new Stack<>();
         int i=0;
-        while (i < s.length() && s.charAt(i) != ' ') {
-            if (s.charAt(i) == '+' || s.charAt(i) == '-' || s.charAt(i) == '(') {
-                operatorStack.push(s.charAt(i));
-                i++;
-            } else if (s.charAt(i) == ')') {
-                while (!operatorStack.isEmpty()&&operatorStack.peek() != '(') {
-                    int n = numberStack.pop();
-                    int m = numberStack.pop();
-                    if (operatorStack.peek() == '-') {
-                        numberStack.push(m - n);
-                    } else {
-                        numberStack.push(m + n);
-                    }
-                    operatorStack.pop();
-                }
-                // "("出栈
-                operatorStack.pop();
-
-                while (!operatorStack.isEmpty() && operatorStack.peek() != '(') {
-                    int n = numberStack.pop();
-                    int m = numberStack.pop();
-                    if (operatorStack.peek() == '-') {
-                        numberStack.push(m - n);
-                    } else {
-                        numberStack.push(m + n);
-                    }
-                    operatorStack.pop();
-                }
-                i++;
-            } else {
-                int num=0;
-                while (i < s.length() && Character.isDigit(s.charAt(i))) {
-                    num = num * 10 + (s.charAt(i) - '0');
+        while (i < s.length()) {
+            if (s.charAt(i) != ' ') {
+                if (s.charAt(i) == '+' || s.charAt(i) == '-' || s.charAt(i) == '(') {
+                    operatorStack.push(s.charAt(i));
                     i++;
-                }
-                numberStack.push(num);
-                while (!operatorStack.isEmpty() && operatorStack.peek() != '(') {
-                    int n = numberStack.pop();
-                    int m = numberStack.pop();
-                    if (operatorStack.peek() == '-') {
-                        numberStack.push(m - n);
-                    } else {
-                        numberStack.push(m + n);
+                } else if (s.charAt(i) == ')') {
+                    while (!operatorStack.isEmpty() && operatorStack.peek() != '(') {
+                        int n = numberStack.pop();
+                        int m = numberStack.pop();
+                        if (operatorStack.peek() == '-') {
+                            numberStack.push(m - n);
+                        } else {
+                            numberStack.push(m + n);
+                        }
+                        operatorStack.pop();
                     }
+                    // "("出栈
                     operatorStack.pop();
-                }
 
+                    while (!operatorStack.isEmpty() && operatorStack.peek() != '(') {
+                        int n = numberStack.pop();
+                        int m = numberStack.pop();
+                        if (operatorStack.peek() == '-') {
+                            numberStack.push(m - n);
+                        } else {
+                            numberStack.push(m + n);
+                        }
+                        operatorStack.pop();
+                    }
+                    i++;
+                } else {
+                    int num = 0;
+                    while (i < s.length() && Character.isDigit(s.charAt(i))) {
+                        num = num * 10 + (s.charAt(i) - '0');
+                        i++;
+                    }
+                    numberStack.push(num);
+                    while (!operatorStack.isEmpty() && operatorStack.peek() != '(') {
+                        int n = numberStack.pop();
+                        int m = numberStack.pop();
+                        if (operatorStack.peek() == '-') {
+                            numberStack.push(m - n);
+                        } else {
+                            numberStack.push(m + n);
+                        }
+                        operatorStack.pop();
+                    }
+
+                }
+            } else {
+                i++;
             }
         }
         return numberStack.peek();
