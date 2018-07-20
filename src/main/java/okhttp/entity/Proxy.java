3b22740c08bdfe2f5ac00af8226210eda3e33129
cliuyide <934392789@qq.com>
2018-07-20 10:22:34 +0800

    新增

---
+++ src/main/java/okhttp/entity/Proxy.java
@@ -0,0 +1,23 @@
+package okhttp.entity;
+
+public class Proxy {
+    private String ip;
+    private String port;
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+}
